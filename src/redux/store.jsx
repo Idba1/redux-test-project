d1eceb0834c2597d4a97183cef6166169784687c
Idba1 <moniraislamself@gmail.com>
2025-01-10 18:26:25 +0600

    redux logger

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import reducer from './features/counter/counterSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
-  reducer: {
-    counter: reducer,
-  },
+    reducer: {
+        counter: reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(logger),
 })
 
 export default store;

11f265f1caf627b7836cbbced7789e2fa6dd494c
Idba1 <moniraislamself@gmail.com>
2025-01-10 16:18:50 +0600

    Consuming the state, action dispatch, and payload

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,23 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByValue } from "../redux/features/counter/counterSlice";
 
 const Counter = () => {
-    const {count} = useSelector((state) => state.counter);
-
-    console.log(count);
+    const { count } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
+    // console.log(count);
 
     return (
         <div>
-            <button>Increment</button>
+            <button onClick={() => dispatch(incrementByValue(5))}>Increment by 5</button>
+            <br />
+            <button onClick={() => dispatch(increment())}>Increment</button>
             <div>
                 <h1>
                     {count}
                 </h1>
             </div>
-            <button>Decrement</button>
-        </div>
+            <button onClick={() => dispatch(decrement())} > Decrement </button>
+        </div >
     );
 };
 
